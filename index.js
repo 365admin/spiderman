c977de4b343c5d24672be0ef29063e2921ed32a6
NielsGregers <niels@timeboxer.dk>
2016-01-07 19:43:53 +0100

    corrected baseurl

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 var lib = require("./crawlers/theworlds50best.com/lib");
 var data = require("./crawlers/theworlds50best.com/spec/data");
 
-console.log(lib.details('www.theworlds50best.com',data.details));
+console.log(lib.details('http://www.theworlds50best.com',data.details));
 process.exit(0)
 
