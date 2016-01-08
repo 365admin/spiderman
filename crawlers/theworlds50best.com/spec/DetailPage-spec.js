166c19b0e24d5c4a31e804f3d36b1c007747acbb
NielsGregers <niels@timeboxer.dk>
2016-01-08 18:33:32 +0100

    fixed test specs

--- crawlers/theworlds50best.com/spec/DetailPage-spec.js
+++ crawlers/theworlds50best.com/spec/DetailPage-spec.js
@@ -1,12 +1,9 @@
 var data = require("./data")
 var top100 = require('../lib.js');
 
-
-
-
 describe("Getting details of a restaurant", function () {
-    it("should get data on a restaurant "), function () {
-        var details = top100.details(data.baseurl,data.details);
-        expect(details.name).toBe('Piazza Duomos');
-    }
+    it("should get data on a restaurant ", function () {
+        var restaurant = top100.details(data.baseurl, data.details);
+        expect(restaurant.name).toBe('Piazza Duomo');
+    });
 })
